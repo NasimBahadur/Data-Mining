6647bb887048624c2b82f665c534b06971e22b4c
Nasim Bahadur <43060004+NasimBahadur@users.noreply.github.com>
2022-05-04 13:46:47 +0600

    Add files via upload

---
+++ Data Collection and Processing/batchfile.bat
@@ -0,0 +1,16 @@
+Python DCP.py -a FP -d mushroom.txt -p 0.3 -rt -m
+Python DCP.py -a AP -d mushroom.txt -p 0.3 -rt -m
+Python DCP.py -a FP -d mushroom.txt -p 0.35 -rt -m
+Python DCP.py -a AP -d mushroom.txt -p 0.35 -rt -m
+Python DCP.py -a FP -d mushroom.txt -p 0.4 -rt -m
+Python DCP.py -a AP -d mushroom.txt -p 0.4 -rt -m
+Python DCP.py -a FP -d mushroom.txt -p 0.45 -rt -m
+Python DCP.py -a AP -d mushroom.txt -p 0.45 -rt -m
+Python DCP.py -a FP -d kosarak.txt -p 0.03 -rt -m
+Python DCP.py -a AP -d  kosarak  .txt -p 0.03 -rt -m
+Python DCP.py -a FP -d kosarak  .txt -p 0.035 -rt -m
+Python DCP.py -a AP -d kosarak  .txt -p 0.035 -rt -m
+Python DCP.py -a FP -d kosarak  .txt -p 0.04 -rt -m
+Python DCP.py -a AP -d  kosarak  .txt -p 0.04 -rt -m
+Python DCP.py -a FP -d kosarak  .txt -p 0.045 -rt -m
+Python DCP.py -a AP -d kosarak  .txt -p 0.045 -rt -m
